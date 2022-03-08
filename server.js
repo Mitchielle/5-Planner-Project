723ef99025b6e6468aa10624b9fa1020df7703e5
Mitchielle <94457005+Mitchielle@users.noreply.github.com>
2022-03-08 08:11:01 +0000

    commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
 var mysqlApostrophe = require("mysql-apostrophe")
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000;
 require('dotenv').config()
@@ -65,7 +66,9 @@ app.use('/', startroutes)
 app.use('/goals', goalroutes)
 app.use('/user', routes)
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "login.ejs"));
+  });
 
 
 app.listen(PORT, () => {
